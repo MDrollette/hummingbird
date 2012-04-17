3e21357fc5fcd67a2b9ce31c0885e09d1487ba70
jdorn <jeremy@jeremydorn.com>
2012-04-16 22:16:42 -0700

    Added ability to use ssl for tracking and dashboard.  Cleaned up source.

--- server.js
+++ server.js
@@ -1,60 +1,84 @@
 var http = require('http'),
+  https = require('https'),
   weekly = require('./lib/weekly'),
   config = require('./config/config'),
   dgram = require('dgram'),
-  static = require('node-static'),
+  node_static = require('node-static'),
   sio = require('socket.io'),
   mongo = require('mongodb'),
   Hummingbird = require('./lib/hummingbird').Hummingbird;
 
-db = new mongo.Db('hummingbird', new mongo.Server(config.mongo_host, config.mongo_port, {}), {});
+var db = new mongo.Db('hummingbird', new mongo.Server(config.mongo_host, config.mongo_port, {}), {});
 
-db.addListener("error", function(error) {
+db.addListener("error", function (error) {
   console.log("Error connecting to mongo -- perhaps it isn't running?");
 });
 
-db.open(function(p_db) {
+db.open(function (p_db) {
   var hummingbird = new Hummingbird();
-  hummingbird.init(db, function() {
-    var server = http.createServer(function(req, res) {
+  hummingbird.init(db, function () {
+    var server_callback = function (req, res) {
       try {
         hummingbird.serveRequest(req, res);
-      } catch(e) {
+      } catch (e) {
         hummingbird.handleError(req, res, e);
       }
-    });
-    server.listen(config.tracking_port, "0.0.0.0");
+    },
+      server;
 
+    if (config.https) {
+      server = http.createServer(server_callback);
+    }
+    else {
+      server = https.createServer({'key': config.https_key, 'cert': config.https_cert},server_callback);
+    }
+    
+    if (config.tracking_ip) {
+      server.listen(config.tracking_port,config.tracking_ip);
+    }
+    else {
+      server.listen(config.tracking_port);
+    }
+    console.log('Tracking server running at http'+(config.https? 's' : '')+'://'+(config.tracking_ip || '*')+':'+config.tracking_port+'/tracking_pixel.gif');
 
-    if(config.enable_dashboard) {
-      var file = new(static.Server)('./public');
+    if (config.enable_dashboard) {
+      var file = new(node_static.Server)('./public');
 
-      var dashboardServer = http.createServer(function (request, response) {
+      var dashboardServer_callback = function (request, response) {
         request.addListener('end', function () {
           file.serve(request, response);
         });
-      });
+      };
+
+      var dashboardServer;
+
+      if (config.https) {
+        dashboardServer = http.createServer(dashboardServer_callback);
+      }
+      else {
+        dashboardServer = https.createServer({'key': config.https_key, 'cert': config.https_cert},dashboardServer_callback);
+      }
 
       dashboardServer.listen(config.dashboard_port);
+      console.log('Dashboard server running at http'+(config.https? 's' : '')+'://*:'+config.dashboard_port);
 
-      io = sio.listen(dashboardServer);
-
-      console.log('Dashboard server running at http://*:' + config.dashboard_port);
-    } else {
-      io = sio.listen(server);
+      console.log('Web Socket server running at ws://*:'+config.dashboard_port);
+    } 
+    else {
+      console.log('Web Socket server running at ws://*:'+config.tracking_port);
     }
+    
+    var io = sio.listen(dashboardServer || server);
 
     io.set('log level', 2);
 
     hummingbird.io = io;
     hummingbird.addAllMetrics(io, db);
 
-    console.log('Web Socket server running at ws://*:' + config.tracking_port);
-
-    if(config.udp_address) {
+    if (config.udp_address) {
       var udpServer = dgram.createSocket("udp4");
 
-      udpServer.on("message", function(message, rinfo) {
+      udpServer.on("message", function (message, rinfo) {
         console.log("message from " + rinfo.address + " : " + rinfo.port);
 
         var data = JSON.parse(message);
@@ -66,6 +90,4 @@ db.open(function(p_db) {
       console.log('UDP server running on UDP port ' + config.udp_port);
     }
   });
-
-  console.log('Tracking server running at http://*:' + config.tracking_port + '/tracking_pixel.gif');
 });

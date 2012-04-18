318d0a71027426420d032740017b37f79d527645
jeremy <user@user-desktop>
2012-04-18 13:09:43 -0700

    Updating client tracking code to not rely on jQuery.  Replaced default metrics with e-commerce related ones (e.g. view shipping info, view billing info, view checkout, complete purchase).

--- client/hummingbird.js
+++ client/hummingbird.js
@@ -1,18 +1,39 @@
 HummingbirdTracker = {};
 
-HummingbirdTracker.track = function(env) {
+HummingbirdTracker.setTrackingServer = function(server) {
+	this.server = server;
+}
+
+HummingbirdTracker.prepareVars = function(env) {
+	var params = [];
+	for(var i in env) {
+		params.push(i+'='+escape(env[i]));
+	}
+	return params.join('&');
+}
+
+HummingbirdTracker.track = function(env, cachebust) {
+  if(!this.server) return false;
+	
   delete env.trackingServer;
   delete env.trackingServerSecure;
   env.u = document.location.href;
   env.bw = window.innerWidth;
   env.bh = window.innerHeight;
-  env.guid = document.cookie.match(/guid=([^\_]*)_([^;]*)/)[2];
-  env.gen = document.cookie.match(/gender=([^;]*);/)[1];
-  env.uid = document.cookie.match(/user_id=([^\_]*)_([^;]*)/)[2];
 
   if(document.referrer && document.referrer != "") {
     env.ref = document.referrer;
   }
 
-  $('body').append('<img src="http://localhost:8000/tracking.gif?' + jQuery.param(env) + '"/>');
+  //cache buster
+  if(cachebust) env.f = Math.ceil(Math.random() * 1000000);
+  
+  var img = document.createElement('img');
+  img.width = '1';
+  img.height = '1';
+  img.src = this.server + 'tracking.gif?' + this.prepareVars(env);
+  
+  document.body.appendChild(img);
+  
+  return false;
 };
